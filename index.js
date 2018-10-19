d4d11b0da0f9ea0632044e89841ad41b651b1778
Mary Palumbo <mpalumbo@conductor.com>
2018-10-19 00:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(numberOfFeet) {
   if (numberOfFeet <= 400) {
     return 'This one is on me!'
-  } else if (numberOfFeet > 2000) {
+  } else if (numberOfFeet > 2000 && numberOfFeet < 2500) {
     return 'I will gladly take your thirty bucks.'
   } else if (numberOfFeet > 2500) {
     return 'No can do.'
